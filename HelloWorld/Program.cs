c8e7c6754e011c964b35a03e787941aa83320182
Jason D'Oyley <jdfd.dev@icloud.com>
2024-02-05 06:39:48 -0700

    Inital commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+        // Data Structures
+        string[] myGroceryArray = new string[2];
+
+        myGroceryArray[0] = "Chicken Breast";
+        myGroceryArray[1] = "Broccoli";
+
+        Console.WriteLine(myGroceryArray[0]);
+    }
+}
