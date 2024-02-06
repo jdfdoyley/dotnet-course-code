287e90dfb5f28967370a12dc33466edfeb98b0ce
Jason D'Oyley <jdfd.dev@icloud.com>
2024-02-05 20:28:00 -0700

    Learn about declaring and initializing variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,37 +11,48 @@ class Program
         // form of data structure and provides fast access to its items by using an index. Arrays are defined with a
         // specific length, and once created, the size cannot be changed.
 
-        int[] numbers = new int[5]; // declare an array of integers with 5 elements
-        int[] initializedNumbers = { 1, 2, 3, 4, 5 }; // declares and initilizes an array
+        // int[] numbers = new int[5]; // declare an array of integers with 5 elements
+        // int[] initializedNumbers = { 1, 2, 3, 4, 5 }; // declares and initilizes an array
 
-        Console.WriteLine();
+        // Console.WriteLine();
 
         // Data Structure - Lists
-        List<double> temperatures = new List<double> { 20.5, 22.3, 18.7, 19.5, 25.1, 21.3, 23.2 };
-        foreach (var temperature in temperatures)
-        {
-            Console.WriteLine(temperature);
-        }
-        Console.WriteLine();
+        // List<double> temperatures = new List<double> { 20.5, 22.3, 18.7, 19.5, 25.1, 21.3, 23.2 };
+        // foreach (var temperature in temperatures)
+        // {
+        //     Console.WriteLine(temperature);
+        // }
+        // Console.WriteLine();
 
         // Add new temperature
-        temperatures.Add(24.4);
-        foreach (var temperature in temperatures)
-        {
-            Console.WriteLine(temperature);
-        }
-        Console.WriteLine();
+        // temperatures.Add(24.4);
+        // foreach (var temperature in temperatures)
+        // {
+        //     Console.WriteLine(temperature);
+        // }
+        // Console.WriteLine();
 
         // Remove specific temperature
-        temperatures.Remove(20.5);
-        foreach (var temperature in temperatures)
-        {
-            Console.WriteLine(temperature);
-        }
-        Console.WriteLine();
+        // temperatures.Remove(20.5);
+        // foreach (var temperature in temperatures)
+        // {
+        //     Console.WriteLine(temperature);
+        // }
+        // Console.WriteLine();
 
         // Calculate average temperature
-        double averageTemp = temperatures.Average();
-        Console.WriteLine(averageTemp);
+        // double averageTemp = temperatures.Average();
+        // Console.WriteLine(averageTemp);
+
+        // Store and retrieve data using literal and variable values in C#
+
+        // Variables
+        int age = 30;  // Declaring and initializing an integer variable
+        double height = 5.9;  // Declaring and initializing a double variable
+        string name = "John"; // Declear and initializing a string variable
+
+        Console.WriteLine(age);
+        Console.WriteLine(height);
+        Console.WriteLine(name);
     }
 }
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d7b5b183226d4179733cd2a04be848bfc8d271")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7dd80c7c2b91858db366113350274a0303951e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
